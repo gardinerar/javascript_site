e298985e211fa84dcb0f92f4ba3c071edddd4f12
Allison Gardiner <allie.gardiner@gmail.com>
2017-03-18 17:29:53 +0000

    new set-up for fibonacci page with sliders and buttons

--- scratchpad.js
+++ scratchpad.js
@@ -1,45 +1,11 @@
-var docTitle = "Computing Sequences";
-document.title = docTitle;
-
-function addDiv (className) {
-  var div = document.createElement("div");
-  div.className = className;
-  // var text = document.createTextNode("This is div" + n);
-  // div.appendChild(text);
-  document.body.appendChild(div);
-}
-
-function addHeader (text) {
-  var header = document.createElement("h1");
-  var content = document.createTextNode(text);
-  header.appendChild(content);
-  document.body.appendChild(header);
-  return text;
-}
-
-function addHyperlink (url, sequenceName) {
-  var p = document.createElement("p");
-  var a = document.createElement("a");
-  var text1 = "(See ";
-  var text2 = " for more information about " + sequenceName + " sequences)";
-  a.text = "here";
-  a.href = url;
-  p.innerHTML = text1;
-  p.appendChild(a);
-  p.innerHTML += text2;
-  document.body.appendChild(p);
-}
-
-addHeader("Fibonacci Sequence");
-addHyperlink("https://oeis.org/A000045","Fibonacci");
-addDiv("stuff-box red shadowed");
-addHeader("Pell Sequence");
-addHyperlink("https://oeis.org/A000129","Pell");
-addDiv("stuff-box blue shadowed");
-addHeader("Tribonacci Sequence");
-addHyperlink("https://oeis.org/A000073","Tribonacci");
-addDiv("stuff-box green shadowed");
-
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
 function fibHelper(n) {
 	var value;
 	var div = document.createElement('div');
@@ -82,15 +48,16 @@ var fib = function (n, node) {
 		//node.style = "display: inline-block;";
 	  node.setAttribute("id", "fib");
 }
+
 var style = document.createElement('style');
 style.textContent = 
 	"#fib {" +
 	"	display: inline-block;" +
-	"	width: 35000px;" +
+	"	width: 20000px;" +
 	"}" +
 	"" +
 	".fib {" +
-	"	background-color: rgba(0,200,255,0.1);" +
+	"	background-color: rgba(0,0,255,0.1);" +
 	"}" +
 	"" +
 	".fib-left {" +
@@ -100,12 +67,6 @@ style.textContent =
 	"}" +
 	"" +
 	".fib-right {" +
-	"	float: right;" +
-	"	display: inline-block;" +
-	"	margin-left: 4px;" +
-	"}" +
-	"" +
-	".fib-middle {" +
 	"	float: right;" +
 	"	display: inline-block;" +
 	"	margin-left: 4px;" +
@@ -130,7 +91,7 @@ style.textContent =
 	"" +
 	".red {" +
 	"	border-color: rgb(255,0,0);" +
-	"	background:   rgb(180,80,80);" +
+	"	background:   rgb(180,60,60);" +
 	"	box-shadow: 1px 1px 2px rgba(200,0,0,0.4);" +
 	"}" +
 	"" +
@@ -142,115 +103,54 @@ style.textContent =
 	"" +
 	".blue {" +
 	"	border-color: rgb(0,0,255);" +
-	"	background:   rgb(60,80,180);" +
+	"	background:   rgb(60,60,180);" +
 	"	box-shadow: 1px 1px 2px rgba(0,0,200,0.4);" +
 	"}" +
 	"" +
 	".green {" +
 	"	border-color: rgb(0,255,0);" +
-	"	background:   rgb(60,160,60);" +
+	"	background:   rgb(60,180,60);" +
 	"	box-shadow: 1px 1px 2px rgba(0,200,0,0.4);" +
 	"}";
 
 document.querySelector('body').appendChild(style);
 
-fib(11, document.querySelector('.red'));
-
-function pellHelper(n) {
-	var value;
+// IIFE = Immediately-invoked function expression
+// An anonymous function which we create, invoke, then throw away.
+// It exists to meet these aims:
+//   0) Avoid clobbering an existing variable named "div" in the global namespace. I don't have
+//      another variable called "div" today, but this protects my webpage in the event that I
+//      later incorporate other JavaScript programs from elsewhere.
+//   1) Collect a few related program statements and keep them together in a single unit.
+// https://en.wikipedia.org/wiki/Immediately-invoked_function_expression
+( function(color, id) {
 	var div = document.createElement('div');
-	div.setAttribute("class", "fib");
+	div.setAttribute('class', color + ' shadowed stuff-box');
+	div.setAttribute('id', id);
+	document.body.appendChild(div);
+}('red', 'fib'));
 
-	// leaf nodes aka. base case
-	if (n < 2) {
-		if (n === 0) {
-			value = 0;
-		} 
-		else if (n === 1) {
-			value = 1;
-		}
-		var p = document.createElement('p');
-		p.textContent = 'Pell(' + n + ') = ' + value;
-		div.appendChild(p)
-	} 
-	else {
-		var left = pellHelper(n - 1);
-		var clas = left.html.getAttribute("class");
-		left.html.setAttribute("class", clas + " fib-left");
-		
-		var right = pellHelper(n - 2);
-		clas = right.html.getAttribute("class");
-		right.html.setAttribute("class", clas + " fib-right");
+fib(9, document.querySelector('.red'))
 
-		value = 2 * (left.value) + right.value;
-		var p = document.createElement('p');
-		p.textContent = 'Pell(' + n + ') = ' + value;
-		div.appendChild(p);
 
-		div.appendChild(left.html);
-		div.appendChild(right.html);
+// divMakerMaker() is a function which returns a function
+// divMakerMaker() takes two arguments and creates a function which requires
+// no arguments of its own, but upon invocation "remembers" the functions it
+// was created with
+var divMakerMaker = function(color, id) {
+	return function() {
+		var div = document.createElement('div');
+		div.setAttribute('class', color + ' shadowed stuff-box');
+		div.setAttribute('id', id);
+		document.body.appendChild(div);
 	}
-	return { 'value': value, 'html': div };
-}
-var pell = function (n, node) {
-	var tree = pellHelper(n)
-		node.appendChild(tree.html);
-		//node.style = "display: inline-block;";
-	  node.setAttribute("id", "fib");
 }
 
-pell(11, document.querySelector('.blue'));
+var blueDiv = divMakerMaker('blue', 'fib');
+var yellowDiv = divMakerMaker('yellow', 'yomama');
 
-function tribHelper(n) {
-	var value;
-	var div = document.createElement('div');
-	div.setAttribute("class", "fib");
+blueDiv();
+yellowDiv();
 
-	// leaf nodes aka. base case
-	if (n < 3) {
-		if (n === 0) {
-			value = 0;
-		} 
-		else if (n === 1) {
-			value = 0;
-		}
-		else if (n === 2) {
-			value = 1;
-		}
-		var p = document.createElement('p');
-		p.textContent = 'Trib(' + n + ') = ' + value;
-		div.appendChild(p)
-	} 
-	else {
-		var left = tribHelper(n - 1);
-		var clas = left.html.getAttribute("class");
-		left.html.setAttribute("class", clas + " fib-left");
-		
-    var middle = tribHelper(n - 2);
-		clas = middle.html.getAttribute("class");
-		middle.html.setAttribute("class", clas + " fib-middle");
-		
-		var right = tribHelper(n - 3);
-		clas = right.html.getAttribute("class");
-		right.html.setAttribute("class", clas + " fib-right");
-		
-		value = left.value + right.value + middle.value;
-		var p = document.createElement('p');
-		p.textContent = 'Trib(' + n + ') = ' + value;
-		div.appendChild(p);
-
-		div.appendChild(left.html);
-		div.appendChild(middle.html);
-		div.appendChild(right.html);
-	}
-	return { 'value': value, 'html': div };
-}
-var trib = function (n, node) {
-	var tree = tribHelper(n)
-		node.appendChild(tree.html);
-		//node.style = "display: inline-block;";
-	  node.setAttribute("id", "fib");
-}
-
-trib(11, document.querySelector('.green'));
-
+fib(10, document.querySelector('.blue'))
+fib(11, document.querySelector('.yellow'))
